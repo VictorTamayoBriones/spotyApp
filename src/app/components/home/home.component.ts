b9a113495ab1a6d2561b1f635b1d8581dac83a8c
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-06 21:52:58 -0500

    albums

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor() { }
+  constructor( private spotify: SpotifyService ) { 
 
-  ngOnInit(): void {
+    this.spotify.getNewReleases();
+
   }
 
 }
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http:HttpClient ) { 
+
+    console.log('servicio listo');
+
+  }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQC_LmNauRrlO12JnRFK2Cdwu1wmV2aQgezjCrkagTB0ZMaRWLDGnXu7iaKv2O02eia6Vky_ZfWR5k68oH0'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+        .subscribe( data =>{
+          console.log(data);
+        });
+
+  }
+}
